18db12bc3fdc1c16a0c892a6210887903875cb20
alexandresgv <strapacao@gmail.com>
2019-06-15 04:00:12 +0100

    criacao do metodo post temperaturas

--- raspberry.js
+++ raspberry.js
@@ -10,22 +10,23 @@ app.post('/api/temperatures/', function (req, res) {
   // Pegando os paramentos recebidos pelo metodo post
   var thermometer_id = req.body.thermometer_id;
   var value = req.body.value;
-  
+  var ip = req.connection.remoteAddress;
+
+  //registrando informacoes recebidas na console
+  console.log(ip, " enviou thermometer_id = " , thermometer_id  , " e  value = ", value ); 
+
   //enviando request ao Laravel
-  axios.post('http://127.0.0.1:8000/api/temperatures/', {
+  axios.post('http://192.168.15.8:8000/api/temperatures/', {
     thermometer_id: thermometer_id,
     value: value 
   })
   .then(function (response) {
-    console.log(response);
+    console.log(response.data.data);
   })
-
-
-  console.log(thermometer_id);
-  console.log(value);
-  res.send("Temperatura enviada com sucesso!");
+  
+  res.send("Raspberry: recebi sua temperatura e enviei ao laravel com sucesso!");
 });
 
 app.listen(3000, function () {
-  console.log('Gateway casa inteligente online...');
+  console.log('Raspberry: Gateway da casa inteligente online na porta 3000...');
 });
